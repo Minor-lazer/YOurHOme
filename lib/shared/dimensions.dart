1b318eb500cfaa25f1c59b2f458e0a504eca1884
Sanjit Sarkar <ssanjit856@gmail.com>
2020-04-09 22:25:43 +0530

    added login and search screen

--- lib/shared/dimensions.dart
+++ lib/shared/dimensions.dart
@@ -1,1 +1,12 @@
-const double card_size = 130;
+import 'package:flutter/cupertino.dart';
+
+// const double card_size = 130;
+double h(BuildContext context)
+{
+  return MediaQuery.of(context).size.height;
+}
+
+double w(BuildContext context)
+{
+  return MediaQuery.of(context).size.width;
+}
---
+++ lib/widgets/CardSearch.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../shared/colors.dart';
+import '../shared/dimensions.dart';
+class CardSearch extends StatelessWidget {
+  String image,name,address;
+  int mprice,disc,price;
+CardSearch({Key key,this.image,this.name,this.address,this.mprice,this.disc});
+
+  @override
+  Widget build(BuildContext context) {
+      return Card(
+          margin: EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
+          elevation: 5,
+          color:Primary,
+          child: Column(children: <Widget>[
+
+          Container(
+          height: h(context)/6,
+          width:w(context),
+          decoration: BoxDecoration(
+            color: Accent,
+            image: DecorationImage(image: AssetImage(this.image),
+            fit: BoxFit.cover)
+          ),
+          
+          ),
+          Container(
+
+          padding: EdgeInsets.symmetric(vertical:10,horizontal:10),
+          decoration: BoxDecoration(
+            color:Colors.white,
+           
+            border: Border(bottom: BorderSide(color: Primary,width: 3.0),)
+          ),
+          // height:h(context)/10,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+children: <Widget>[
+  
+  Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+    Text('${this.name}',style:TextStyle(color:Primary,fontSize: 20,fontWeight: FontWeight.w500)),
+    Text('${this.address}',style:TextStyle(color:Primary.withOpacity(.8),fontSize: 15,fontWeight: FontWeight.w400)),
+    Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('₹${(this.mprice - (this.mprice/100)).floor()}',style:TextStyle(color:Primary,fontSize: 20,fontWeight: FontWeight.w500)),
+        SizedBox(width:10),
+    Text('₹${this.mprice}',style:TextStyle(color:Primary,fontSize: 15,fontWeight: FontWeight.w400,decoration: TextDecoration.lineThrough)),
+    ],)
+  ],),
+  RaisedButton(onPressed: ()
+  {
+
+  },
+  splashColor: Accent,
+
+  padding: EdgeInsets.symmetric(vertical:10,horizontal:15),
+  shape:RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(20.0))),
+  color:Primary,
+
+  child: Text('${this.disc}% Off',style:TextStyle(color:Colors.white,fontSize: 20,fontWeight: FontWeight.w800)),
+  )
+],
+          ),
+          ),
+        
+        ],));
+      
+  }
+ 
+}
